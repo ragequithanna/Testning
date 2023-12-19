2344ad8564e649dd2997eb0e24dd7394fbac0690
ragequithanna <hannastoor@outlook.com>
2023-12-19 09:40:39 +0100

    hjbhjg

--- src/Start.java
+++ src/Start.java
@@ -1,10 +1,27 @@
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
 public class Start {
-    public static void main(String[] args) {
+    private static InfDB idb;
+    public static void main(String[] args) throws InfException {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 StartWindow startWindow = new StartWindow();
                 startWindow.setVisible(true);
             }
         });
+        
+    
+    try {
+        idb = new InfDB("mibdb", "3360", "mibdba", "mibkey");
+    
+    } catch (InfException ex) {
+        Logger.getLogger(Start.class.getName()).log(Level.SEVERE, null, ex);
+    }
+
+    } 
     }
 }
